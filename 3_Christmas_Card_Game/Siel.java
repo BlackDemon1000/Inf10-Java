3eeea1c60be0e9e080c15b290cb9cf8242db4781
Christoph Beier <100367685+BlackDemon1000@users.noreply.github.com>
2024-01-15 10:43:48 +0100

    new game mechanics

---
+++ 3_Christmas_Card_Game/Siel.java
@@ -0,0 +1,9 @@
+public class Spiel {
+    private KARTE karte1;
+    private KARTE karte2;
+    private KARTE karte3;
+    private int punkteSpieler;
+    public Spiel(){
+        //Konstructorrumpf
+    }
+}
