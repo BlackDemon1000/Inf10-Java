b79da2868a2dc1f6d0817b46938898a008ffb7c3
Christoph9575 <GW1_10@gmx.de>
2024-02-26 08:26:01 +0100

    added project

---
+++ 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
@@ -0,0 +1,36 @@
+
+
+public class Spiel extends Ereignisbehandlung
+{
+  private Berge berg1;
+  private Insekt insekt1;
+  private Himmel himmel1;
+  private Wiese wiese1;
+  private Vogel vogel1;
+  private Berge berg2;
+  private Insekt insekt2;
+ 
+  
+  
+  public Spiel(){
+    himmel1 = new Himmel();
+    wiese1 = new Wiese();
+    berg1 = new Berge();
+    insekt1 = new Insekt(1);
+    vogel1 = new Vogel();
+    berg2 = new Berge();
+    insekt2 = new Insekt(1);
+    berg1.PositionSetzen(270, 315);
+    insekt1.PositionSetzen(435, 192);
+    berg2.PositionSetzen(533, 400);
+    insekt2.PositionSetzen(637, 221);
+    wiese1.NachHintenBringen();
+    himmel1.NachHintenBringen();
+      
+  }
+  @Override
+  public void TaktImpulsAusführen(){
+      berg1.Bewegen();
+      insekt1.Bewegen();
+  }
+}
