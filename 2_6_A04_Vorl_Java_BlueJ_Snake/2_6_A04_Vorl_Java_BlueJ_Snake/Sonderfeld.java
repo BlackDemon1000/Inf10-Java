09eefa5c6a7c3e5730f5dc2df6db985f647ebd8c
Christoph Beier <100367685+BlackDemon1000@users.noreply.github.com>
2024-06-12 11:07:06 +0200

    Sonderfeld

--- 2_6_A04_Vorl_Java_BlueJ_Snake/2_6_A04_Vorl_Java_BlueJ_Snake/Sonderfeld.java
+++ 2_6_A04_Vorl_Java_BlueJ_Snake/2_6_A04_Vorl_Java_BlueJ_Snake/Sonderfeld.java
@@ -1,10 +1,21 @@
 
 public class Sonderfeld
 {
-
-    public Sonderfeld()
-    {
-
+    protected Spiel spiel;
+    protected SonderSymbol symbol;
+    public Sonderfeld(Spiel s) {
+        spiel = s;
     }
 
+    public int XPositionGeben() {
+        return symbol.XPositionGeben();
+    }
+
+    public int YPositionGeben() {
+        return symbol.YPositionGeben();
+    }
+
+    public void Aktion() {
+        //amgus, Methode wird von Unterklassen überschrieben
+    }
 }
